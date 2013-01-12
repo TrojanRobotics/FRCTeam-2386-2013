bcdf2fee06814ae1364d98bc04395809fea6f12a
Matt Bettinson <mattbettinson@me.com>
2013-01-12 14:21:48 -0500

    Initial commit
    Completely empty

---
+++ src/edu/wpi/first/wpilibj/templates/Robot.java
@@ -0,0 +1,30 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) FIRST 2008. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package edu.wpi.first.wpilibj.templates;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+public class Robot extends IterativeRobot {
+
+    public void robotInit() {
+
+    }
+
+    public void autonomousPeriodic() {
+
+    }
+    
+    public void teleopPeriodic() {
+        
+    }
+    
+    public void testPeriodic() {
+    
+    }
+    
+}
