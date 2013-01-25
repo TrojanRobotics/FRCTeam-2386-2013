2201a6f624fc4025a2d1cc8d923dc74c962ef93c
Nick McGuire <pudgeball.mcguire@gmail.com>
2013-01-25 14:51:52 -0500

    Cleaned up the repo
    Now more logical and a better gitignore file

---
+++ Java/2013/src/com/BCHS/Bot.java
@@ -0,0 +1,31 @@
+package com.BCHS;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+public class Bot extends IterativeRobot
+{
+	public void robotInit()
+	{
+		
+	}
+	
+	public void disabledPeriodic()
+	{
+		
+	}
+	
+	public void autonomousPeriodic()
+	{
+		
+	}
+	
+	public void teleopPeriodic()
+	{
+		
+	}
+	
+	public void testPeriodic()
+	{
+		
+	}
+}
