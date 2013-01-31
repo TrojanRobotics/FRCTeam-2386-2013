e22aa8eba09168486125eaa3a487c0a4cb38bd78
Taylor <zu.nk.12@hotmail.com>
2013-01-31 15:34:28 -0500

    Added code
    Added code that will connect with the chasis code to move the robot with
    the xbox conroller.

--- Java/2013/src/com/BCHS/Bot.java
+++ Java/2013/src/com/BCHS/Bot.java
@@ -1,7 +1,9 @@
 package com.BCHS;
 
 import com.BCHS.misc.XboxController;
+import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.IterativeRobot;
+
 
 public class Bot extends IterativeRobot
 {
@@ -25,7 +27,17 @@ public class Bot extends IterativeRobot
 	
 	public void teleopPeriodic()
 	{
-		system.out.println("");
+		double x = Controller.getX();
+		double y = Controller.getY();
+		
+		x = Lib.signSquare(x);
+		y = Lib.signSquare(y);
+		
+		Chasis.leftSide.set(Lib.limitOutput(y - x));
+		Chasis.rightSide.set(-Lib.limitOutput(y + x));
+			
+		System.out.println(x);
+		System.out.println(y);
 	}
 	
 	public void testPeriodic()
