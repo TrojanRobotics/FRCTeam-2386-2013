26780b81de1b70341d1b7d52c74e345ff51d1ad0
Laxman Sooriyathas <laxman0202@gmail.com>
2013-01-31 15:30:20 -0500

    Added Drive Code
    Added Chasis and Bundle files and added code to config file.

--- Java/2013/src/com/BCHS/Config.java
+++ Java/2013/src/com/BCHS/Config.java
@@ -2,5 +2,8 @@ package com.BCHS;
 
 public class Config
 {
-    
+    static double LEFT_SIDE_ENCODER_DPP = 0.0;
+	static double RIGHT_SIDE_ENCODER_DPP = 0.0;
+	
+	static double [] CHASIS_PID = {1.0, 0.0, 0.0};
 }
