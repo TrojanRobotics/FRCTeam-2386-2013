832e3aa9f52ab0989fa7ee2b380fa907bd66ed29
Laxman Sooriyathas <laxman0202@gmail.com>
2013-02-02 15:19:38 -0500

    Added Climber Class
    This should work, but it may need some fixes....yolo swag 2012

---
+++ Java/2013/src/com/BCHS/Climber.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.BCHS;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.Jaguar;
+
+/**
+ *
+ * @author Laxman
+ */
+public class Climber 
+{
+	Encoder climbEncoder;
+	Jaguar climbJaguar;
+	PIDController climbPID;
+	double Kp, Ki, Kd;
+	
+	public Climber(int aChannel, int bChannel, int jagChannel)
+	{
+		Kp = 1.0;
+		Ki = 0.0;
+		Kd = 0.0;
+		
+		climbEncoder = new Encoder(aChannel, bChannel);
+		climbEncoder.setDistancePerPulse(0.1337);
+		climbEncoder.setPIDSourceParameter(Encoder.PIDSourceParameter.kDistance);
+		climbEncoder.start();
+		
+		climbJaguar = new Jaguar(jagChannel);
+		
+		climbPID = new PIDController(Kp, Ki, Kd, climbEncoder, climbJaguar);	
+	}
+	
+	public void setSpeed(double speed)
+	{
+		climbJaguar.set(speed);
+	}
+	
+	public void stop()
+	{
+		climbPID.disable();
+		climbJaguar.disable();
+	}
+}
