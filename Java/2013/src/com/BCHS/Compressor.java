de7642e70763f3baaa520c14dd086de645c2fbae
Adam Gilchrist <sxe.jesus@gmail.com>
2013-02-07 19:03:25 -0500

    Revert "Revert "compressor added""
    This reverts commit c759b24b5cf269ee99f0c61344f4c6b72c6449f4.

---
+++ Java/2013/src/com/BCHS/Compressor.java
@@ -0,0 +1,25 @@
+package com.BCHS;
+
+
+public class Compressor {
+    
+    public Compressor(int pressureSwitchSlot, int pressureSwitchChannel, int compressorRelaySlot, int compressorRelayChannel)
+    {
+        
+    }
+    
+    public boolean getPressureSwitchValue(boolean switchValue)
+    {
+        return false;
+    }
+    
+    public void start()
+    {
+        
+    }
+    
+    public void stop()
+    {
+        
+    }
+}
