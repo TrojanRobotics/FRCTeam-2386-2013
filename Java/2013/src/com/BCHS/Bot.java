152fe9050c643babc07d0b923057744c4ae0b43f
Taylor <zu.nk.12@hotmail.com>
2013-02-09 14:11:10 -0500

    final changes before testing

--- Java/2013/src/com/BCHS/Bot.java
+++ Java/2013/src/com/BCHS/Bot.java
@@ -40,10 +40,6 @@ public class Bot extends IterativeRobot
         retrieval = new Retrieval(Config.RETRIEVAL_CHANNEL);
 		climber = new Climber(Config.CLIMBER_CHANNEL);
 		
-		Kp = 0.1;
-		Ki = 0.0;
-		Kd = 0.0;
-		
 		printData();
 		
 	}
@@ -96,8 +92,6 @@ public class Bot extends IterativeRobot
 		
 		chasis.leftSide.set(Lib.limitOutput(y - x));
 		chasis.rightSide.set(-Lib.limitOutput(y + x));
-		
-		chasis.driveSolenoid.set(true);
 		
 		if (secondaryJoystick.getTrigger())
 			shooter.set(1.0);
@@ -175,11 +169,11 @@ public class Bot extends IterativeRobot
 		
 		
                 
-
+/*
         if (chasis.compressor.getPressureSwitchValue()) 
 			chasis.compressor.start();
         else
-			chasis.compressor.stop();                         
+			chasis.compressor.stop();   */                      
 	}
 
 	public void testPeriodic()
